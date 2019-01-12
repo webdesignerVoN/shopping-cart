3a13cbab9004a322cf24c0aca035ab262ba01648
VoN <webdesignervon@gmail.com>
2019-01-12 17:47:21 +0100

    Imported Decouple to Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
