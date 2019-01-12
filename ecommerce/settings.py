917df9f6a27310c112e6b7a5fe513ec959674f58
VoN <webdesignervon@gmail.com>
2019-01-12 17:27:42 +0100

    Security changes with Python Decouple

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wirs5i*zm8wawgkf220ejh^recx#$7xz)$p_t+=fdgdwm-y)8e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
