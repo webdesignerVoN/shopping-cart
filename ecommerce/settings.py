fe032e419684a3a0393d5b69424c4c94544f185f
VoN <webdesignervon@gmail.com>
2019-01-12 18:04:32 +0100

    Added Allowed Hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'von-cart.herokuapp.com']
 
 
 # Application definition
